ec2921e63e9be0b5833de3c03e79c0d6fdd23246
zhangsan <ss@hfuusec.cn>
2016-01-20 18:50:36 +0800

    getipV2.0

--- getIP.py
+++ getIP.py
@@ -1,11 +1,11 @@
 #coding:utf-8
 #ss
-#python getIP.py Bdlog.txt
+#python getIP.py Bdlog.txt bdip.txt
 #能够提取ip地址，并且可以去重
 import re
 from sys import argv
 
-script, file_name = argv
+script, file_name, out_File = argv
 
 def read_file(file_name):
 	_fLog = open(file_name)
@@ -14,20 +14,21 @@ def read_file(file_name):
 	sep = '\n'
 	while _fLine:
 		reip = re.compile(r'(?<![\.\d])(?:\d{1,3}\.){3}\d{1,3}(?![\.\d])')
-		print 'ss'
+		#print 'ss'
 		
 		#ip = reip.search(_fLine).group()#字符串ip
 		ip = reip.findall(_fLine)#列表ip
-		print ip
+		#print ip
 		if ip not in ips:
 			ips.append(ip)
 		_fLine = _fLog.readline()
-	out = open("Bdip.txt","a")
+	out = open(out_File,"a")
 	for iptmp in ips:
 		ipstr = str(iptmp)
 		out.write(ipstr+sep)
 	out.close()
 	_fLog.close()
+	print "the work done!"
 
 
 read_file(file_name)
